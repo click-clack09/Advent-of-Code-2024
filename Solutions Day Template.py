8f2e5d9b128cfde55750cb1affa3603821c19e07
click_clack09 <77758597+click-clack09@users.noreply.github.com>
2024-12-04 20:34:09 -0600

    merging

---
+++ Solutions Day Template.py
@@ -0,0 +1,23 @@
+import re
+import os
+import math
+
+#Advent of code Day X puzzle 1
+
+def part_one(data):
+    pass
+    return
+
+
+#Advent of code Day X puzzle 2
+    
+def part_two(data):
+    pass
+    return
+
+read_file = open("input.txt",'r')
+data = read_file.read()
+print(part_one(data))
+print(part_two(data))
+read_file.close()
+    
