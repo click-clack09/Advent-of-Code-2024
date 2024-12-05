2e7adf13741e8a773f153bc75095edcdd5ea33fd
click_clack_09 <77758597+click-clack09@users.noreply.github.com>
2024-12-04 19:44:13 -0600

    testing local git connection 2nd attempt

---
+++ day_04/Solutions Day 4.py
@@ -0,0 +1,22 @@
+import re
+import os
+
+#Advent of code Day 4 puzzle 1
+
+def part_one(data):
+    pass
+    return
+
+
+#Advent of code Day 4 puzzle 2
+    
+def part_two(data):
+    pass
+    return
+
+read_file = open("input.txt",'r')
+data = read_file.read()
+print(part_one(data))
+print(part_two(data))
+read_file.close()
+    
